8b2c15e8b37e421dfade8522e99ddde255c92ae3
Zak EVANS <19ZE1858@barton.ac.uk>
2020-11-17 11:45:26 +0000

    added intial values

--- NEAprototype/Graph.vb
+++ NEAprototype/Graph.vb
@@ -13,10 +13,31 @@ Public Class Graph
     Private minibatchLABLE(,) As Integer
 
     Sub New(ByVal isnewnetwork As Boolean, ByVal networksize As Integer())
+        Dim nullarray() As Double = {}
+        ReDim activations(networksize.Length)
+        ReDim biases(networksize.Length - 1)
+        ReDim zvalues(networksize.Length - 1)
+        ReDim errors(networksize.Length - 1)
+        ReDim weightsmatrix(networksize.Length - 1)
+
         If isnewnetwork Then
+            activations(0) = New vector(networksize(0), nullarray)
+
+            For i = 1 To networksize.Length - 1
+                activations(i) = New vector(networksize(i), nullarray)
+                biases(i - 1) = New vector(networksize(i - 1), nullarray)
+                zvalues(i - 1) = New vector(networksize(i - 1), nullarray)
+                errors(i - 1) = New vector(networksize(i - 1), nullarray)
+                weightsmatrix(i - 1) = New Matrix(networksize(i), networksize(i - 1), True)
+            Next
+
+
+
+
+        Else
 
         End If
-
+        
     End Sub
 
 
@@ -48,36 +69,46 @@ Public Class Graph
 
     End Sub
 
-
-    Function converttoblackandwhite(ByVal inputimage As Bitmap) As Double()
-        Dim pixelcolour As New Color
-        Dim temp As Integer
-        Dim pointer As Integer
-        Dim nodearray(inputimage.Height + 1 * inputimage.Width + 1) As Double
+    Public Function sendtestingdatathroughnetwrok(ByVal inputimage As Bitmap) As Integer
+        Dim pixelcolor As New Color
 
         For y = 0 To inputimage.Height
             For x = 0 To inputimage.Width
 
-                pixelcolour = inputimage.GetPixel(x, y)
-
-                Dim pixelcolourarray = {pixelcolour.A, pixelcolour.R, pixelcolour.G, pixelcolour.B} 'takes the aRGB value for the current pixel
-
-                For i = 1 To 3
-                    temp += pixelcolourarray(i)
-                Next
-                temp = temp / 3
-
-                temp = convertTodouble(temp)
-
-                nodearray(pointer) = temp
             Next
         Next
-
-
-
-        Return nodearray
-
     End Function
+
+
+    'Function converttoblackandwhite(ByVal inputimage As Bitmap) As Double()
+    '    Dim pixelcolour As New Color
+    '    Dim temp As Integer
+    '    Dim pointer As Integer
+    '    Dim nodearray(inputimage.Height + 1 * inputimage.Width + 1) As Double
+
+    '    For y = 0 To inputimage.Height
+    '        For x = 0 To inputimage.Width
+
+    '            pixelcolour = inputimage.GetPixel(x, y)
+
+    '            Dim pixelcolourarray = {pixelcolour.A, pixelcolour.R, pixelcolour.G, pixelcolour.B} 'takes the aRGB value for the current pixel
+
+    '            For i = 1 To 3
+    '                temp += pixelcolourarray(i)
+    '            Next
+    '            temp = temp / 3
+
+    '            temp = convertTodouble(temp)
+
+    '            nodearray(pointer) = temp
+    '        Next
+    '    Next
+
+
+
+    '    Return nodearray
+
+    'End Function
 
 
 
